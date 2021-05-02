ebe62e5eb350942db1f2afdcd46aafcbe9ffd4be
AngelinaZhukovich <angelinka04@mail.ru>
2021-05-02 19:22:30 +0300

    dowFiles

--- uploade.js
+++ uploade.js
@@ -1,4 +1,4 @@
-export function uploade(selector){
+export function uploade(selector, optiions: {} = {}){
     const input = document.querySelector(selector);
 
 
@@ -6,10 +6,20 @@ export function uploade(selector){
     open.classList.add('btn');
     open.textContent = 'Открыть';
 
+    if (optiions.multi) {
+        input.setAttribute (qualifiedName: 'multiple', value: true)
+    }
+
     input.insertAsjacentElement (position: 'afterend', open);
 
 
+    const triggerInput = () => input.click ()
 
+    const changeHandler = event => {
+        console.log(event)
+    }
 
     open.addEventListener (type: 'click', triggerInput)
+
+    input.addEventListener ('change', changeHandler)
 }
